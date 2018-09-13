f377568fa8de8de1de781773304d432fb32b1739
Yasser Altamimi <yosy2010@gmail.com>
2018-09-14 02:47:37 +0300

    Initial Commit

---
+++ VowelMovement/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VowelMovement
+//
+//  Created by YASSER ALTAMIMI on 14/09/2018.
+//  Copyright © 2018 YASSER ALTAMIMI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

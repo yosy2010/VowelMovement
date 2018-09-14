2acfeb7354bba4577eceb6160c550c3f3b443d7e
Yasser Altamimi <yosy2010@gmail.com>
2018-09-14 17:48:04 +0300

    first part done

--- VowelMovement/main.m
+++ VowelMovement/main.m
@@ -19,11 +19,37 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Orignal Strings: %@", orignal);
         
         // array for devoowelized strings
-        NSMutableArray *devoowelized = [NSMutableArray array];
+        NSMutableArray *devoowelizedStrings = [NSMutableArray array];
         
         // array for the list of the vowels
         NSArray *vowles = @[@"a", @"e", @"i", @"o", @"u"];
         
+        // declare a block that will devoowelize strings and return it to the devoowelizedStrings array
+        void(^devoowelizer)(id, NSUInteger, BOOL*);
+        
+        devoowelizer = ^(id string, NSUInteger i, BOOL *stop) {
+            
+            // create an NSMutableString to add the sting and adgust it in it
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+            
+            //iterate over the array of vowles and replace occurences of vowles with an empty string
+            for (NSString *string in vowles) {
+                
+                // create a range of the length of the string
+                NSRange range = NSMakeRange(0, [string length]);
+                
+                // replace the vowles with empty strings
+                [newString replaceOccurrencesOfString:string
+                                           withString:@""
+                                              options:NSCaseInsensitiveSearch
+                                                range:range];
+            
+            }
+            
+            // add the string to the devoowelizedStrings array
+            [devoowelizedStrings addObject:newString];
+            
+        };
     }
     return 0;
 }
